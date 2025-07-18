4cd611634debfcccc547696df00b6de393c5b936
Jedd Nugent <jeddnugent@gmail.com>
2025-07-18 17:26:46 +1000

    Implementing ImageGrid on main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import ImageGrid from './components/ImageGrid';
 import './styles/App.css';
 
 function App() {
@@ -6,6 +7,28 @@ function App() {
   return (
     <>
       <Header />
+      <ImageGrid
+        imageOneUrl="src/assets/concert_shot_1.png"
+        imageTwoUrl="src/assets/concert_shot_2.png"
+        imageThreeUrl="src/assets/portait_shot_1.png"
+        imageFourUrl="src/assets/concert_shot_5.png"
+        imageFiveUrl="src/assets/concert_shot_6.png"
+        imageSixUrl="src/assets/concert_shot_7.png"
+        imageSevenUrl="src/assets/concert_shot_8.png"
+        imageEightUrl="src/assets/concert_shot_9.png"
+        imageNineUrl="src/assets/concert_shot_3.png"
+      />
+      <ImageGrid
+        imageOneUrl="src/assets/concert_shot_15.png"
+        imageTwoUrl="src/assets/concert_shot_11.png"
+        imageThreeUrl="src/assets/concert_shot_16.png"
+        imageFourUrl="src/assets/concert_shot_13.png"
+        imageFiveUrl="src/assets/concert_shot_10.png"
+        imageSixUrl="src/assets/concert_shot_18.png"
+        imageSevenUrl="src/assets/concert_shot_19.png"
+        imageEightUrl="src/assets/concert_shot_20.png"
+        imageNineUrl="src/assets/concert_shot_21.png"
+      />
     </>
   );
 }
