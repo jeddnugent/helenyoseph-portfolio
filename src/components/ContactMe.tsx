d877f927d2a76ec62bbfff034312f6a9d2616b13
Jedd Nugent <jeddnugent@gmail.com>
2025-08-18 17:29:33 +1000

    Adding support for mobile layouts
    Adding new layouts for mobile to better showcase images on smaller screens

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,4 +1,4 @@
-import ContactImage from "../assets/concert_shot_12.png";
+// import ContactImage from "../assets/concert_shot_12.png";
 import "../styles/ContactMe.css";
 import instagramLogo from "../assets/instagram_logo.png";
 
@@ -6,7 +6,6 @@ function ContactMe() {
 	return (
 		<>
 			<div className="contactme-container">
-				<img src={ContactImage} alt="Logo" />
 				<div className="contactme-text-container">
 					<h1>Please reach out for booking information: </h1>
 					<a href="mailto:helenyoseph@gmail.com" target="_blank" rel="noopener noreferrer">
