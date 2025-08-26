b2ae7ff51b061670fb7e2f2c892e43f019c1aeac
Jedd Nugent <jeddnugent@gmail.com>
2025-08-26 14:50:22 +1000

    Moving all large images to public rather than within the src bundle

--- src/interfaces/gridImages1.ts
+++ src/interfaces/gridImages1.ts
@@ -1,12 +1,12 @@
-import image1 from "../assets/concert_shot_1_full.jpg";
-import image2 from "../assets/concert_shot_2_full.jpg";
-import image3 from "../assets/portait_shot_1_full.jpg";
-import image4 from "../assets/concert_shot_5_full.jpg";
-import image5 from "../assets/concert_shot_6_full.jpg";
-import image6 from "../assets/concert_shot_7_full.jpg";
-import image7 from "../assets/concert_shot_8_full.jpg";
-import image8 from "../assets/concert_shot_9_full.jpg";
-import image9 from "../assets/concert_shot_3_full.jpg";
+import image1 from "/concert_shot_1_full.jpg";
+import image2 from "/concert_shot_2_full.jpg";
+import image3 from "/portait_shot_1_full.jpg";
+import image4 from "/concert_shot_5_full.jpg";
+import image5 from "/concert_shot_6_full.jpg";
+import image6 from "/concert_shot_7_full.jpg";
+import image7 from "/concert_shot_8_full.jpg";
+import image8 from "/concert_shot_9_full.jpg";
+import image9 from "/concert_shot_3_full.jpg";
 
 const imageArray = [image1, image2, image3, image4, image5, image6, image7, image8, image9];
 
