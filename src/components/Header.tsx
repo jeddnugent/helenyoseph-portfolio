4a9d05fee2ec85dcec5dd28fdc1d24a717e4cfdc
Jedd Nugent <jeddnugent@gmail.com>
2025-09-04 14:35:23 +1000

    Implementing Booking scroll button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 // @ts-expect-error vite image conversion
-import headerImage from "../assets/crowd.jpg?w=768;1280;1920&format=webp&as=srcset";
+import headerImage from "../assets/1.jpg?w=768;1280;1920&format=webp&as=srcset";
 import "../styles/Header.css";
 
 function Header() {
@@ -10,13 +10,17 @@ function Header() {
 					srcSet={headerImage}
 					sizes="(max-width: 768px) 768px, (max-width: 1280px) 1280px, 1920px"
 					alt="Header"
-					loading="lazy"
+					loading="eager"
 					width="1920"
 					height="900"
 				/>
 				<div className="text-container">
 					<h1>helen</h1>
 					<h2>ሄለን</h2>
+
+				</div>
+				<div className="bookings-btn__wrapper">
+					<a href="#bookings" className="bookings-btn">Bookings</a>
 				</div>
 			</div>
 		</>
